7674522fc26c4333b05fe73045e42e1a3abe04dd
kthT <138095558+2176003915th@users.noreply.github.com>
2025-03-25 19:56:10 +0900

    25/03/25
    start

---
+++ spring-maple/src/main/java/com/induk/spring_maple/SpringMapleApplication.java
@@ -0,0 +1,13 @@
+package com.induk.spring_maple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMapleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMapleApplication.class, args);
+	}
+
+}
---
+++ spring-maple/src/test/java/com/induk/spring_maple/SpringMapleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.induk.spring_maple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMapleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
